b630d77a9b348717f2313b215d76eec2e939d765
Willanov <willanov@yandex.ru>
2023-06-10 13:42:29 +0500

    Third

---
+++ Third/Program.cs
@@ -0,0 +1,13 @@
+﻿void Cubes(int N)
+{
+int Count=1;
+   for (int i=0;i<N;i++)
+{
+    Console.WriteLine(Math.Pow(Count,3));
+    Count++;
+} 
+}
+Console.Clear();
+Console.Write("Введите число N =>  ");
+int N=Convert.ToInt32(Console.ReadLine());
+Cubes(N);
