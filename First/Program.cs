2161920d88acfd587b5b14a2bc496afa123324be
Willanov <willanov@yandex.ru>
2023-06-10 13:16:38 +0500

    First no string

--- First/Program.cs
+++ First/Program.cs
@@ -1,24 +1,25 @@
-﻿string reverse(string Number)
+﻿int reverse(int Number)
 {
-char[] mas= new char[5]; 
+int temp=0;
+int[] mas= new int[5]; 
 int count = 4;
  for (int i =0; i<5;i++)
  {
-    mas[i]=Number[count];
-    count--;
+    temp=Number%10;
+    mas[i]=temp;
+    Number=Number/10;
  }
- string NewNumber = "";
- for (int i=0;i<5;i++)
+ int NewNumber = mas[0];
+ for (int i=1;i<5;i++)
  {
-    NewNumber+=mas[i];
+    NewNumber=NewNumber*10+mas[i];
  }
 return NewNumber;
 }
 Console.Clear();
 Console.WriteLine("Введите пятизначное число => ");
-string N =Console.ReadLine();
-int check = Convert.ToInt32(N);
-if (check<10000){Console.WriteLine($"Число {N} не является пятизначным");}
+int N =Convert.ToInt32(Console.ReadLine());
+if (N<10000){Console.WriteLine($"Число {N} не является пятизначным");}
 System.Console.WriteLine($"{N} => {reverse(N)}");
 if (reverse(N)==N){Console.WriteLine("Число является палиндромом");}
 else {Console.WriteLine("Число не является палиндромом");}
